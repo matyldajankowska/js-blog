bc6e81a9665d4b86e7532056beee36891d041f45
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-02-28 09:31:50 +0100

    remove active class

--- js/script.js
+++ js/script.js
@@ -7,7 +7,19 @@ function titleClickHandler(){
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
   /* add class 'active' to the clicked link */
+
+  const activeArticles = document.querySelectorAll('.posts .active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* remove class 'active' from all articles */
 

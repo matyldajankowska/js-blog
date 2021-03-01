e7942778b84a89a19f23d3abea4aee5ed04228e0
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-03-01 10:14:25 +0100

    fix js code

--- js/script.js
+++ js/script.js
@@ -3,22 +3,20 @@
 function titleClickHandler(event){
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
-  console.log(event);
-  
+
 
   /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
+  for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
   /* add class 'active' to the clicked link */
   
   clickedElement.classList.add('active');
-  console.log('clickedElement:', clickedElement);
+  
 
   /* remove class 'active' from all articles */
 
@@ -31,12 +29,10 @@ for(let activeLink of activeLinks){
   /* get 'href' attribute from the clicked link */
 
   const articleSelector = clickedElement.getAttribute("href");
-  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   const targetArticle = document.querySelector (articleSelector);
-  console.log (targetArticle);
 
   /* add class 'active' to the correct article */
 
@@ -44,11 +40,9 @@ for(let activeLink of activeLinks){
 
 }
 
-const links = document.querySelectorAll('.titles a');
+  const links = document.querySelectorAll('.titles a');
 
-console.log(links);
-
-const optArticleSelector = '.post',
+  const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
@@ -66,16 +60,11 @@ function generateTitleLinks(){
 
   const articles = document.querySelectorAll(optArticleSelector);
 
-  for (let article of articles) {
-    article.querySelector(optArticleSelector);
-  }
-
-
   for(let article of articles){
 
     /* get the article id */
 
-  const articleId = article.getAttribute('id');
+   const articleId = article.getAttribute('id');
 
     /* find the title element */
 
